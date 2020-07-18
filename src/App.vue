f0f1b55e3b7a6ba26ffc04596ccdd4217ef6344b
kimagurekappa <tomoya0799@gmail.com>
2020-07-18 22:25:28 +0900

    20200718

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,5 @@
 <template>
   <div id="app">
-  <tab-menu></tab-menu>
-    <div class="hello">
     <h1>{{ msg }}</h1>
     <h3>Git Hub へのアクセス</h3>
     <ul>
@@ -10,25 +8,43 @@
     <h3>You Tube へのアクセス</h3>
     <ul>
       <li><a href="https://www.youtube.com/channel/UC3M75vH5Y0kizWKaT2BnNyQ?view_as=subscriber" target="_blank" rel="noopener">You Tube</a></li>
-
     </ul>
-  </div>
+
+      <el-menu :default-active="activeIndex" mode="horizontal" router>
+      <el-menu-item index="home" :route="{ name:'home' , path: '/home' }">Home</el-menu-item>
+      <el-menu-item index=about :route="{ name:'about' , path: '/about' }">About</el-menu-item>
+    </el-menu>
+    <router-view />
+
   </div>
 </template>
 
 <script>
-import TabMenu from '@/components/TabMenu'
+
 
 export default {
   data () {
     return {
-      msg: 'Welcome to home page for KimagureKappa!'
+      msg: 'きまぐれかっぱ',
+      name: 'app',
+      activeIndex: '',
     }
   },
-  name: 'App',
-  components: {
-    TabMenu
+  mounted() {
+      // 読み込まれたページをアクティブ
+      this.activeIndex = this.$route.name;
   }
 }
 
 </script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: left;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,16 +1,27 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import About from '@/components/About'
 
 Vue.use(Router)
 
 export default new Router({
-  mode: 'history',  // 追記 URLに#が入るのを防止
+  mode: 'history', 
   routes: [
     {
-        path: '/hello',
-        name: 'HelloWorld',
-        component: HelloWorld
+      path: '/',
+      name: 'HelloWorld',
+      component: HelloWorld
+    },
+    {
+      path: '/home',
+      name: 'home',
+      component: HelloWorld
+    },
+    {
+      path: '/about',
+      name: 'about',
+      component: About
     }
   ]
 })
